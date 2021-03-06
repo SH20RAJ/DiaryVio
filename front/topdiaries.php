f4ad65422383c781c1101c8ddc94fa4addb5e489
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2021-03-06 21:44:57 +0530

    Add files via upload

---
+++ front/topdiaries.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+  <title>Top Diaries</title>
+  <link rel="stylesheet" href="http://localhost/DiaryVio/front/main.css">
+  </head>
+
+<body>
+<div class="post">
+<a href="http://localhost/DiaryVio/D/1.php">
+<div class="title">My First Post</div>
+<hr>
+<img src="http://localhost/DiaryVio/D/thumbs/1.php.jpg" width="100%"  >
+</a>
+</div>
+
+
+</body>
+</htmL>
+
